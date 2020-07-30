4d7a81ac952239c6c67e47a44d42c458f51b6eef
Team Bacon <nguyenlamtuong2001@gmail.com>
2020-07-30 14:02:18 +0700

    Install maven and json data implement

--- src/com/Tuong/MedXMain/MedXMain.java
+++ src/com/Tuong/MedXMain/MedXMain.java
@@ -1,7 +1,44 @@
 package com.Tuong.MedXMain;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class MedXMain {
 	public static void main(String[] args) {
-		
+		//First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+         
+        JSONObject employeeObject = new JSONObject(); 
+        employeeObject.put("employee", employeeDetails);
+         
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+         
+        JSONObject employeeObject2 = new JSONObject(); 
+        employeeObject2.put("employee", employeeDetails2);
+         
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+         
+        //Write JSON file
+        try (FileWriter file = new FileWriter("Data/employees.json")) {
+ 
+            file.write(employeeList.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 }
