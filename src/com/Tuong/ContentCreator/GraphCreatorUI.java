4b322d05990da7675318c8f6cf198a359ca52f8f
Team Bacon <nguyenlamtuong2001@gmail.com>
2020-08-20 15:09:34 +0700

    Added dialog for graph creator

--- src/com/Tuong/ContentCreator/GraphCreatorUI.java
+++ src/com/Tuong/ContentCreator/GraphCreatorUI.java
@@ -8,7 +8,7 @@ import javax.swing.JTextField;
 public class GraphCreatorUI extends BasicUI{
 	private PatientUI pUI;
 	public GraphCreatorUI(PatientUI pUI) {
-		super("Create new graph", new Dimension(100,100), false, null);
+		super("Create new graph", new Dimension(300,100), false, null);
 		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		this.pUI = pUI;
 	}
@@ -19,7 +19,11 @@ public class GraphCreatorUI extends BasicUI{
 		createButton("Create graph", this, new ButtonAction() {
 			@Override
 			public void click() {
-				if(pUI.createNewGraph(name.getText(), unit.getText())) setVisible(false);
+				if(pUI.createNewGraph(name.getText(), unit.getText())) {
+					showDialog("Success", "You just created "+name.getText()+" graph", 1);
+					setVisible(false);
+				}else 
+					showDialog("Fail", name.getText()+" exists", 0);
 			}
 		});
 	}
